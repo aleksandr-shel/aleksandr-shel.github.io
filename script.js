798774e872b29dc174b38a8337b4d915b5fb44fa
Aleksandr Shelukheev <shelukheev@gmail.com>
2022-05-05 23:08:23 -0400

    updated

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-let divs = document.querySelectorAll('.section-item')
+let sectiondivs = document.querySelectorAll('.section-item')
 
 let resume = document.querySelector('.section-item-resume')
 
-divs.forEach(div=>{
+let navitems = document.querySelectorAll('.nav-link');
+
+function onVisisble(element, callback){
+    new IntersectionObserver((entries, observer)=>{
+        entries.forEach(entry => {
+            if(entry.intersectionRatio > 0) {
+              callback(element);
+            //   observer.disconnect();
+            }
+          });
+    }).observe(element);
+}
+
+function changeActiveLink(navitemId){
+    navitems.forEach(item => {
+        item.classList.remove('active')
+        if (item.getAttribute("href").substring(1) === navitemId){
+
+            item.classList.add('active')
+        }
+    })
+}
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    console.log(rect.top + rect.bottom)
+    return (
+        rect.top + rect.bottom >= 0.5 * window.innerHeight
+    );
+}
+
+sectiondivs.forEach(div=>{
     onVisisble(div, (div)=>{
         div.classList.add('animation-fadein')
         setTimeout(() => {
@@ -17,15 +48,3 @@ onVisisble(resume, (resume)=>{
         resume.classList.remove('animation-fadein')
     }, 1000);
 })
-
-
-function onVisisble(element, callback){
-    new IntersectionObserver((entries, observer)=>{
-        entries.forEach(entry => {
-            if(entry.intersectionRatio > 0) {
-              callback(element);
-            //   observer.disconnect();
-            }
-          });
-    }).observe(element);
-}
