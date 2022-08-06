005939eb2eaf34d085df7c3e975ed46c715a8d33
Aleksandr Shelukheev <60274747+aleksandr-shel@users.noreply.github.com>
2022-08-06 04:42:32 -0400

    updated

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // add class navbarDark on navbar scroll
-// const header = document.querySelector('.navbar');
-// console.log(header)
-// window.onscroll = function() {
-//     const top = window.scrollY;
-//     if(top >=100) {
-//         header.classList.add('navbarDark');
-//     }
-//     else {
-//         header.classList.remove('navbarDark');
-//     }
-// }
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    console.log(top)
+    if(top >=100) {
+        header.classList.remove('displayNone')
+    }
+    else {
+        header.classList.add('displayNone')
+    }
+}
 // collapse navbar after click on small devices
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
