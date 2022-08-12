4eed2ec739f14f8c1d46536575d0d20299cdf023
Aleksandr Shelukheev <shelukheev@gmail.com>
2022-08-12 14:46:56 -0400

    updated

--- script.js
+++ script.js
@@ -40,4 +40,56 @@ let i = 0;
 //     // console.log(cursor_x, cursor_y)
 // }, 1000);
 
-console.log('hello')
+
+// bubbles handler later
+const bubbles = document.querySelectorAll('.wrapper > div:not(.container-fluid, .shark)');
+
+bubbles.forEach(bubble=>{
+    bubble.addEventListener('click', (e)=>{
+        console.log(e.target);
+    })
+})
+
+
+//scrollable appearance
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
